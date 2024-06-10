615a3a3fd678262e346c6709cb2f910f8dacbdb6
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-06-10 21:03:34 +0900

    debugging asm

--- src/concurrent/multitask/README.md
+++ src/concurrent/multitask/README.md
@@ -2,3 +2,37 @@
 
 - github: [oreilly-japan/conc_ytakano/chap6](https://github.com/oreilly-japan/conc_ytakano/tree/main/chap6)
 
+## Run
+
+### C
+
+Compile:
+
+```bash
+gcc asm/context.S -c -fPIC -ggdb -o asm/context.o
+ar cruUs asm/libcontext.a asm/context.o
+
+# gcc main.c -Lasm -lcontext -o main
+gcc -S main.c -o main.s
+gcc -c main.s -o main.o
+gcc main.s -Lasm -lcontext -o main
+```
+
+Run:
+
+```bash
+./main
+```
+
+### LLDB
+
+```bash
+lldb main
+break set -n main
+break set -n set_context
+run
+s
+register read
+memory read -f x -c 1 $rsp
+```
+
---
+++ src/concurrent/multitask/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct registers {
+  unsigned long rbx;
+  unsigned long rbp;
+  unsigned long r12;
+  unsigned long r13;
+  unsigned long r14;
+  unsigned long r15;
+  unsigned long rsp;
+  unsigned long rdx;
+};
+
+uint64_t set_context(struct registers *regs);
+// void switch_context(struct registers *regs) __attribute__((noreturn));
+
+int main(void) {
+  struct registers regs = {
+    .rbx = 0,
+    .rbp = 0,
+    .r12 = 0,
+    .r13 = 0,
+    .r14 = 0,
+    .r15 = 0,
+    .rsp = 0,
+    .rdx = 0
+  };
+
+  uint64_t ret = set_context(&regs);
+
+  printf("%ld", ret);
+
+  return 0;
+}
