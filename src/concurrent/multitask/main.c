97111b62c884c4305ca62ab8cf06814f7842f0b7
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-06-12 00:17:57 +0900

    register struct with heap alloc

--- src/concurrent/multitask/main.c
+++ src/concurrent/multitask/main.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct registers {
-  unsigned long rbx;
-  unsigned long rbp;
-  unsigned long r12;
-  unsigned long r13;
-  unsigned long r14;
-  unsigned long r15;
-  unsigned long rsp;
-  unsigned long rdx;
+  uint64_t rbx;
+  uint64_t rbp;
+  uint64_t r12;
+  uint64_t r13;
+  uint64_t r14;
+  uint64_t r15;
+  uint64_t rsp;
+  uint64_t rdx;
 };
 
 uint64_t set_context(struct registers *regs);
 void switch_context(struct registers *regs) __attribute__((noreturn));
 
-int main(void) {
-  struct registers regs = {
-    .rbx = 0,
-    .rbp = 0,
-    .r12 = 0,
-    .r13 = 0,
-    .r14 = 0,
-    .r15 = 0,
-    .rsp = 0,
-    .rdx = 0
-  };
+void nothing() {
+  return;
+}
 
-  uint64_t ret = set_context(&regs);
+int main(void) {
+  struct registers *regs = malloc(sizeof(struct registers));
+  memset(regs, 3, sizeof(struct registers));
+
+  uint64_t ret = set_context(regs);
   printf("%ld", ret);
+
+  free(regs);
 
   return 0;
 }
