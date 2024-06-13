203cd26e320e8d539b06fcc2e471b443cf96cbfe
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-06-13 23:39:24 +0900

    set_context rsp and rdx

--- src/concurrent/multitask/main.c
+++ src/concurrent/multitask/main.c
@@ -3,32 +3,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct registers {
-  uint64_t rbx;
-  uint64_t rbp;
-  uint64_t r12;
-  uint64_t r13;
-  uint64_t r14;
-  uint64_t r15;
-  uint64_t rsp;
-  uint64_t rdx;
+// struct registers {
+//   uint64_t rbx;
+//   uint64_t rbp;
+//   uint64_t r12;
+//   uint64_t r13;
+//   uint64_t r14;
+//   uint64_t r15;
+//   uint64_t rsp;
+//   uint64_t rdx;
+// };
+
+struct structure {
+  char a;
 };
 
-uint64_t set_context(struct registers *regs);
-void switch_context(struct registers *regs) __attribute__((noreturn));
+// uint64_t set_context(struct registers *regs);
+// void switch_context(struct registers *regs) __attribute__((noreturn));
 
-void nothing() {
-  return;
-}
+void nothing() { return; }
+void printer(struct structure *s, int a) { printf("%c %d", s->a, a); }
 
 int main(void) {
-  struct registers *regs = malloc(sizeof(struct registers));
-  memset(regs, 3, sizeof(struct registers));
+  struct structure *s = malloc(sizeof(struct structure));
+  s->a = 'A';
+  int a = 17;
 
-  uint64_t ret = set_context(regs);
-  printf("%ld", ret);
+  int i = 0;
+  for (i = 0; i < 3; i++) {
+    nothing();
+  }
 
-  free(regs);
+  printer(s, a);
+  free(s);
 
+  // struct registers *regs = malloc(sizeof(struct registers));
+  // memset(regs, 3, sizeof(struct registers));
+
+  // uint64_t ret = set_context(regs);
+  // printf("%ld", ret);
+
+  // free(regs);
   return 0;
 }
