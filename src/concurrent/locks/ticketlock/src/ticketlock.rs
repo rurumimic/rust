9cb70ad20e6363bfb568ada1e08023331a9a6506
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-08-17 14:18:14 +0900

    ticketlock impl

---
+++ src/concurrent/locks/ticketlock/src/ticketlock.rs
@@ -0,0 +1,57 @@
+use std::cell::UnsafeCell;
+use std::ops::Deref;
+use std::ops::DerefMut;
+use std::sync::atomic::{fence, AtomicUsize, Ordering};
+
+pub struct TicketLock<T> {
+    ticket: AtomicUsize, // next ticket number
+    turn: AtomicUsize,   // current ticket number
+    data: UnsafeCell<T>,
+}
+
+pub struct TicketLockGuard<'a, T> {
+    ticket_lock: &'a TicketLock<T>,
+}
+
+impl<T> TicketLock<T> {
+    pub fn new(v: T) -> Self {
+        TicketLock {
+            ticket: AtomicUsize::new(0),
+            turn: AtomicUsize::new(0),
+            data: UnsafeCell::new(v),
+        }
+    }
+
+    pub fn lock(&self) -> TicketLockGuard<T> {
+        let t = self.ticket.fetch_add(1, Ordering::Relaxed);
+        // Note: If the turn value becomes higher than the number of threads,
+        // new threads may not be able to access the lock.
+        while self.turn.load(Ordering::Relaxed) != t {}
+        fence(Ordering::Acquire);
+
+        TicketLockGuard { ticket_lock: self }
+    }
+}
+
+impl<'a, T> Drop for TicketLockGuard<'a, T> {
+    fn drop(&mut self) {
+        self.ticket_lock.turn.fetch_add(1, Ordering::Release);
+    }
+}
+
+unsafe impl<T> Sync for TicketLock<T> {}
+unsafe impl<T> Send for TicketLock<T> {}
+
+impl<'a, T> Deref for TicketLockGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ticket_lock.data.get() }
+    }
+}
+
+impl<'a, T> DerefMut for TicketLockGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.ticket_lock.data.get() }
+    }
+}
