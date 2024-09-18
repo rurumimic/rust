e53cb098889308949ea9dfd20dc05389f889fa02
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-09-18 18:56:34 +0900

    anyhow apperror into response

--- src/tokio/backend/README.md
+++ src/tokio/backend/README.md
@@ -9,6 +9,16 @@ cd backend
 
 ```bash
 cargo add axum
+cargo add axum -F macros
 cargo add tokio -F full
+```
+
+### Anyhow
+
+- examples: [anyhow-error-response](https://github.com/tokio-rs/axum/blob/main/examples/anyhow-error-response/src/main.rs)
+- docs.rs: [anyhow](https://docs.rs/anyhow/latest/anyhow/)
+
+```bash
+cargo add anyhow
 ```
 
--- src/tokio/backend/src/main.rs
+++ src/tokio/backend/src/main.rs
@@ -1,9 +1,10 @@
-use axum::serve;
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Html, routing::get, serve, Router};
 use tokio::net::TcpListener;
 
+use backend::errors::AppError;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     let app = Router::new().route("/", get(handler));
 
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
@@ -15,6 +16,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+#[axum::debug_handler]
+async fn handler() -> Result<Html<&'static str>, AppError> {
+    try_thing()?;
+    Ok(Html("<h1>Hello, World!</h1>"))
+}
+
+fn try_thing() -> anyhow::Result<()> {
+    anyhow::bail!("oh no");
 }
