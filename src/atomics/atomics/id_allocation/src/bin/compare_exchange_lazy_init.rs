9ecf50a1308bfcc83b0ad5f5225bd1083ba144fd
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-01-06 08:31:08 +0900

    fix method

--- src/atomics/atomics/id_allocation/src/bin/compare_exchange_lazy_init.rs
+++ src/atomics/atomics/id_allocation/src/bin/compare_exchange_lazy_init.rs
@@ -5,7 +5,7 @@ fn get_key() -> u64 {
     let key = KEY.load(Ordering::Relaxed);
     if key == 0 {
         let new_key = generate_random_key();
-        match KEY.compare_exchange_weak(0, new_key, Ordering::Relaxed, Ordering::Relaxed) {
+        match KEY.compare_exchange(0, new_key, Ordering::Relaxed, Ordering::Relaxed) {
             Ok(_) => new_key,
             Err(v) => v,
         }
