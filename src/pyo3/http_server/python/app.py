f1b3135c5d46abc8d63448549245da711bfcf444
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-03-31 22:15:59 +0900

    fix instance pyo3

--- src/pyo3/http_server/python/app.py
+++ src/pyo3/http_server/python/app.py
@@ -2,6 +2,13 @@ class Parameters:
     name: str
     count: int
 
+    def __init__(self, name: str, count: int):
+        self.name = name
+        self.count = count
+
+    def __str__(self):
+        return f"Parameters(name={self.name}, count={self.count})"
+
 def hello(name: str) -> str:
     return f"Hello, {name}!"
 
--- src/pyo3/http_server/src/main.rs
+++ src/pyo3/http_server/src/main.rs
@@ -6,14 +6,37 @@ fn main() -> PyResult<()> {
         env!("CARGO_MANIFEST_DIR"),
         "/python/app.py"
     )));
+
     let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
         let app: PyResult<Bound<'_, PyModule>> =
-            PyModule::from_code(py, py_app, c_str!(""), c_str!(""));
-        let _class_parameters: Py<PyAny> = app.clone().getattr("Parameters")?.into();
-        let def_hello: Py<PyAny> = app?.getattr("hello")?.into();
-        def_hello.call1(py, ("Rust",))
+            PyModule::from_code(py, py_app, c_str!("app.py"), c_str!("parameters"));
+
+        let class_parameters = app?.getattr("Parameters")?;
+
+        let args = ("Rust", 3);
+        let instance_parameters = class_parameters.call1(args)?;
+
+        let name = instance_parameters.getattr("name")?;
+        let count = instance_parameters.getattr("count")?;
+
+        println!("name: {}", name);
+        println!("count: {}", count);
+
+        Ok(instance_parameters.into())
     });
 
-    println!("py: {}", from_python?);
+    println!("on rust: {}", from_python.as_ref().unwrap());
+    let instance_parameters = from_python.as_ref().unwrap();
+
+    let _ = Python::with_gil(|py| -> PyResult<()> {
+        let result = instance_parameters
+            .call_method0(py, "__str__")?
+            .extract::<String>(py)?;
+
+        println!("on python: {}", result);
+
+        Ok(())
+    });
+
     Ok(())
 }
