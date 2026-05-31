818e18a1e99d4a243f2d53d47cc60a5d1593b4fc
rurumimic <10915235+rurumimic@users.noreply.github.com>
2026-05-31 16:19:46 +0900

    add asserts

--- src/atomics/basics/reference_counting/src/bin/rc_mut.rs
+++ src/atomics/basics/reference_counting/src/bin/rc_mut.rs
@@ -6,11 +6,17 @@ fn main() {
 
     assert!(Rc::ptr_eq(&a, &b));
     assert_eq!(Rc::strong_count(&a), 2);
+    assert_eq!(Rc::strong_count(&b), 2);
     dbg!(a.as_ptr(), b.as_ptr());
+    dbg!(Rc::strong_count(&a), Rc::strong_count(&b));
 
     Rc::make_mut(&mut a).push(4);
 
+    assert!(!Rc::ptr_eq(&a, &b));
     assert_eq!(a.as_slice(), &[1, 2, 3, 4]);
     assert_eq!(b.as_slice(), &[1, 2, 3]);
+    assert_eq!(Rc::strong_count(&a), 1);
+    assert_eq!(Rc::strong_count(&b), 1);
     dbg!(a.as_ptr(), b.as_ptr());
+    dbg!(Rc::strong_count(&a), Rc::strong_count(&b));
 }
